2f0481b184cb288f2879cb1712cc688e17864f12
Amany Siraj Al-Din <amanysirajaldin@email.com>
2021-10-19 03:49:36 +0200

    Create contact-btn component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
+import { ContactBtnComponent } from './contact-btn/contact-btn.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
-    HeaderComponent
+    HeaderComponent,
+    ContactBtnComponent
   ],
   imports: [
     BrowserModule,
