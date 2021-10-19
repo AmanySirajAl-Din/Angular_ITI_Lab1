28b274ee6cbc9c3039d1416c1673ef843537454c
Amany Siraj Al-Din <amanysirajaldin@email.com>
2021-10-19 03:08:06 +0200

    Start update footer template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
 import { HeaderComponent } from './header/header.component';
 
+
 @NgModule({
   declarations: [
     AppComponent,
