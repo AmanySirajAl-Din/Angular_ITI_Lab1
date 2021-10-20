85d8524b63e79caf6f70cc52dbadfec3b43f399b
Amany Siraj Al-Din <amanysirajaldin@email.com>
2021-10-21 00:16:18 +0200

    Add skills Array

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -3,13 +3,20 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.css']
+  styleUrls: ['./skills.component.css'],
 })
 export class SkillsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  skillsArr: Array<object> = [
+    { name: 'HTML', degree: '5' },
+    { name: 'CSS', degree: '4' },
+    { name: 'JavaScript', degree: '5' },
+    { name: 'Angular', degree: '3' },
+    { name: 'React', degree: '3' },
+    { name: 'Bootstrap', degree: '5' },
+    { name: 'API', degree: '2' },
+  ];
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
