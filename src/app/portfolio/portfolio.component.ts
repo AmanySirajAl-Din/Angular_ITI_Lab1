a954bec5ba9bd2b8318b7f8d87ebbb10ee9a885e
Amany Siraj Al-Din <amanysirajaldin@email.com>
2021-10-21 02:23:36 +0200

    Create project cards dynamically

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -3,13 +3,17 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.css']
+  styleUrls: ['./portfolio.component.css'],
 })
 export class PortfolioComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  constructor() {}
+  myProjects: Array<string> = [
+    'Web Design',
+    'Web Development',
+    'Front-End Web Development',
+    'Amany Is Smart',
+    'You Can Do It',
+    'Go For It',
+  ];
+  ngOnInit(): void {}
 }
